731be85085e78babd0d1d70ca685d130e5a1f54b
JamesWillems <james.willems2002@gmail.com>
2023-03-07 00:46:00 +0100

    Car Model

--- ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/model/Car.java
+++ ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/model/Car.java
@@ -1,7 +1,5 @@
 package ca.mcgill.ecse321.ParkingManagement.model;
 
-
-
 import java.util.*;
 import jakarta.persistence.*;
 
@@ -9,7 +7,6 @@ import jakarta.persistence.*;
 @Entity
 public class Car
 {
-
 
   public enum Size { Regular, Large }
 
@@ -46,43 +43,10 @@ public class Car
   {
     return parkingManagementSystem;
   }
-  /* Code from template association_SetOneToMany */
-  public boolean setParkingManagementSystem(ParkingManagementSystem aParkingManagementSystem)
-  {
-    boolean wasSet = false;
-    if (aParkingManagementSystem == null)
-    {
-      return wasSet;
-    }
 
-    ParkingManagementSystem existingParkingManagementSystem = parkingManagementSystem;
-    parkingManagementSystem = aParkingManagementSystem;
-    if (existingParkingManagementSystem != null && !existingParkingManagementSystem.equals(aParkingManagementSystem))
-    {
-      existingParkingManagementSystem.removeCar(this);
-    }
-    parkingManagementSystem.addCar(this);
-    wasSet = true;
-    return wasSet;
-  }
-
-  public void delete()
-  {
-    carsByLicensePlate.remove(getLicensePlate());
-    ParkingManagementSystem placeholderParkingManagementSystem = parkingManagementSystem;
-    this.parkingManagementSystem = null;
-    if(placeholderParkingManagementSystem != null)
-    {
-      placeholderParkingManagementSystem.removeCar(this);
-    }
+  public void setParkingManagementSystem(ParkingManagement aParkingManagementSystem) {
+    this.parkingManagementSystem = aParkingManagementSystem;
   }
 
 
-  public String toString()
-  {
-    return super.toString() + "["+
-            "licensePlate" + ":" + getLicensePlate()+ "]" + System.getProperties().getProperty("line.separator") +
-            "  " + "size" + "=" + (getSize() != null ? !getSize().equals(this)  ? getSize().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
-            "  " + "parkingManagementSystem = "+(getParkingManagementSystem()!=null?Integer.toHexString(System.identityHashCode(getParkingManagementSystem())):"null");
-  }
 }
