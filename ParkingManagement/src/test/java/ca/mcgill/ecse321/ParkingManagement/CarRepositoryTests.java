42e16de0df304b6743fb64738c0376f4c01e0aec
LukeBebee <ljbebee@gmail.com>
2023-03-06 23:06:08 -0500

    add import

--- ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/CarRepositoryTests.java
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/CarRepositoryTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ca.mcgill.ecse321.ParkingManagement.dao.CarRepository;
+
+
 
 @SpringBootTest
 class CarRepositoryTests {
