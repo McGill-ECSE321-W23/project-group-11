d19b13256a76f512ddfc59db031a46a0a5659956
Brian Morava <zmorava515@hotmail.com>
2023-03-07 00:49:26 -0500

    Update EmployeeRepositoryTests.java

--- ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/EmployeeRepositoryTests.java
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/EmployeeRepositoryTests.java
@@ -1,71 +1,62 @@
 package ca.mcgill.ecse321.ParkingManagement;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import ca.mcgill.ecse321.ParkingManagement.dao.EmployeeRepository;
-
-
-import ca.mcgill.ecse321.ParkingManagement.model.Account;
-import ca.mcgill.ecse321.ParkingManagement.model.ParkingManagementSystem;
-import ca.mcgill.ecse321.ParkingManagement.model.Employee;
+import ca.mcgill.ecse321.petadoptionsystem.model.Account;
+import ca.mcgill.ecse321.petadoptionsystem.model.Employee;
+import ca.mcgill.ecse321.petadoptionsystem.model.ParkingManagementSystem;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 
 @SpringBootTest
 public class EmployeeTest {
-
     @Autowired
-    private ParkingManagementSystemRepository parkingManagementRepository;
-
-
+    private ParkingManagementSystem parkingManagementSystemRepository;
     @Autowired
-    private EmployeeRepository EmployeeRepository;
+    private AccountRepository accountRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private ManagerRepository managerRepository;
+
+    @BeforeEach
+    @AfterEach
+    public void clearDataBase(){
+        employeeRepository.deleteAll();
+        customerRepository.deleteAll();
+        accountRepository.deleteAll();
+        parkingManagementSystemRepository.deleteAll();
+        managerRepository.deleteAll();
+    }
 
     /**
      * Employee persistence test
      */
     @Test
-    public void testPersistAndLoadEmployee() {
-        ParkingManagementSystemRepository pas = TestingUtility.initParkingManagementSystemRepository(1);
-        parkingManagementSystemRepository.save(pas);
+    public void persistAndLoadEmployee(){
+        ParkingManagementSystem system = TestingUtility.initparkingManagementSystemRepository(123);
+        parkingManagementSystemRepository.save(system);
 
+        Account account = TestingUtility.initAccount("johndoe1955@gmail.com", "password", system);
+        accountRepository.save(account);
+        account = null;
+        account = accountRepository.findAccountByEmail("johndoe1955@gmail.com");
+        Employee employee = TestingUtility.initAdmin(account, system);
 
-        Employee emplUser = TestingUtility.initEmployee(pas);
+        employeeRepository.save(employee);
+        employee = null;
 
-        emplUser.setSchedule("MTWF");
-
-        regularUserRepository.save(emplUser);
-
-        emplUser = null;
-        
-        
-
-
-        assertNotNull(emplUser);
-
-        assertEquals("MTWF", emplUser.getSchedule());
-
-
+        int emId = accountRepository.findAccountByEmail("johndoe1955@gmail.com").getUserRole().getId();
+        employee = employeeRepository.findAdminById(emId);
+        assertNotNull(employee);
+        assertEquals(emId, employee.getId());
     }
 
-    @BeforeEach
-    @AfterEach
-    public void DeleteDataBase() {
-
-        EmployeeRepository.deleteAll();
-        parkingManagementSystemRepository.deleteAll();
-
-    }
 }
