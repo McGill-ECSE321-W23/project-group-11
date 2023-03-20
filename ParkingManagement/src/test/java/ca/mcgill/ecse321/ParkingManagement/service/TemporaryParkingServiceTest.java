5b9243f2faf526e66c711160b4d3b2095782d1fc
LukeBebee <ljbebee@gmail.com>
2023-03-20 12:20:10 -0400

    starting service test

--- ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/TemporaryParkingServiceTest.java
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/TemporaryParkingServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
@@ -25,12 +26,38 @@ import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
+import ca.mcgill.ecse321.ParkingManagement.dao.LargeTempSpotRepository;
+import ca.mcgill.ecse321.ParkingManagement.dao.RegularTempSpotRepository;
+import ca.mcgill.ecse321.ParkingManagement.model.LargeTempSpot;
 
 
 @ExtendWith(MockitoExtension.class)
 public class TemporaryParkingServiceTest {
 
 //TODO 2.8.2 step 3
+@Mock
+private LargeTempSpotRepository largeTempDao;
+private RegularTempSpotRepository regTempDao;
+
+@InjectMocks
+private TemporaryParkingService service;
+
+private static final int SPOT_KEY = 1;
+
+@BeforeEach
+public void setMockOutput() {
+    lenient().when(largeTempDao.findById(anyInt())).thenAnswer( (InvocationOnMock invocation) -> {
+        if(invocation.getArgument(0).equals(SPOT_KEY)) {
+            LargeTempSpot spot = new LargeTempSpot();
+            spot.setId(SPOT_KEY);;
+            return spot;
+        } else {
+            return null;
+        }
+    });
+}
+
+
 
 
 }
