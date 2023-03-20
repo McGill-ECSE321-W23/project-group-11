0a8a99f01036879dfa94bc8bccb39912574cf54b
LukeBebee <ljbebee@gmail.com>
2023-03-20 13:34:30 -0400

    removed unused imports

--- ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/dao/SystemInfoRepositoryTests.java
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/dao/SystemInfoRepositoryTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.sql.Time;
-import ca.mcgill.ecse321.ParkingManagement.dao.*;
 import ca.mcgill.ecse321.ParkingManagement.model.*;
 
 @SpringBootTest
