94223a703669c5b3cbaa10cbafd15d53200f452b
Mrosati22 <massimo.rosati@mail.mcgill.ca>
2023-03-21 15:59:02 -0400

    updated ServiceType

---
+++ ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/dto/ServiceTypeDto.java
@@ -0,0 +1,55 @@
+package ca.mcgill.ecse321.ParkingManagement.dto;
+import ca.mcgill.ecse321.ParkingManagement.model.Manager;
+
+public class ServiceTypeDto {
+
+    //Contructor
+    public ServiceTypeDto(String name, int cost, int duration, Manager manager) {
+        this.name = name;
+        this.cost = cost;
+        this.duration = duration;
+        this.manager = manager;
+    }
+
+
+    //Getters and Setters
+    private String name;
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    private int cost;
+
+    public void setCost(int value) {
+        this.cost = value;
+    }
+
+    public int getCost() {
+        return this.cost;
+    }
+
+    private int duration;
+
+    public void setDuration(int value) {
+        this.duration = value;
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    private Manager manager;
+
+    public Manager getManager() {
+        return this.manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+}
--- ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/service/ServiceTypeService.java
+++ ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/service/ServiceTypeService.java
@@ -3,15 +3,18 @@ package ca.mcgill.ecse321.ParkingManagement.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.sql.Date;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 import ca.mcgill.ecse321.ParkingManagement.dao.*;
 import ca.mcgill.ecse321.ParkingManagement.model.*;
+import ca.mcgill.ecse321.ParkingManagement.dto.ServiceTypeDto;
 
 @Service
-public class ServiceTypeService {
+public class ServiceTypeService{
+    @Autowired
+    ServiceType serviceTypeRepository;
+    @Autowired
+    ManagerRepository managerRepository;
     
     /**
      * Creates a ServiceType
@@ -24,8 +27,7 @@ public class ServiceTypeService {
      */
 
      @Transactional
-     public SpecificService createServiceType(String name, int cost, int duration, Manager manager) throws Exception{
-        SpecificService specificService;
+     public ServiceType createServiceType(String name, int cost, int duration, Manager manager) throws Exception{
         //check if manager is null
         if(manager == null){
             Exception e = new Exception("Inputted manager was null");
@@ -33,11 +35,87 @@ public class ServiceTypeService {
         }
         //check if the manager is in the database
         if(managerRepository.existsById(manager.getId())){
-            Exception e = new Exception("Manager does not exist");
+            Exception e = new Exception("Manager does not exist in database");
             throw e;
         }
-        
+        //all is good create a service type
+        ServiceType serviceType = new ServiceType();
+        //set attributes
+        serviceType.setName(name);
+        serviceType.setCost(cost);
+        serviceType.setDuration(duration);
+        serviceType.setManager(manager);
 
-        return specificService;
+        return serviceType;
      }
+     /**
+     * Deletes an existing serviceType
+     *
+     * @param serviceType service type to delete
+     * @return void
+     * @throws Exception
+     */
+     @Transactional
+     public void removeServiceType(ServiceType serviceType) throws Exception {
+        //null service type check
+        if(serviceType == null){
+            Exception e = new Exception("Service Type is null");
+            throw e;
+        }
+        //if it exists then delete it
+        if(existsByName(serviceType.getName())){
+            serviceTypeRepository.deleteById(serviceType.getName());
+        }
+        else{
+            //if it doesnt exist throw exception
+            Exception e = new Exception("Inputted service type doesn not exist.");
+            throw e;
+        }
+    }
+    /**
+     * Returns a service type from a name
+     *
+     * @param name of desired spot
+     * @return serivce with corresponding name
+     * @throws Exception
+     */
+    @Transactional
+    public ServiceType getServiceTypeByName(String name) throws Exception {
+        if(name == null){
+            Exception e = new Exception("Inputted name is null.");
+            throw e;
+        }
+        ServiceType serviceType = null;
+        //if the name exists then get it from database
+        if(serviceTypeRepository.existsByName(name)){
+            serviceType = serviceTypeRepository.findServiceTypeByName(name);
+        }
+        //if the name doesnt exist throw exception
+        else{
+            Exception e = new Exception("Service Type does not exist");
+            throw e;
+        }
+        return serviceType;
+    }
+    /**
+     * Returns a list of all service types
+     *
+     * @return list of TempSpots
+     */
+    @Transactional
+    public List<ServiceType> getAllServiceTypes() throws Exception{
+        ServiceTypeDto serviceTypeDto;
+        List<ServiceType> allServiceTypes = new ArrayList<ServiceTypeDto>();
+        //for each service type in the database add it to the list of service types
+        for(ServiceType serviceType : serviceTypeRepository.findAll().get()){
+            serviceTypeDto = convertToDto(serviceType);
+            allServiceTypes.add(serviceTypeDto);
+        }
+        return allServiceTypes;
+    }
+    //converts a Servicetype into a servicetype dto
+    private ServiceTypeDto convertToDto(ServiceType serviceType) {
+        ServiceTypeDto dto = new ServiceTypeDto(serviceType.getName(),serviceType.getCost(),serviceType.getDuration(),serviceType.getManager());
+        return dto;
+    }
 }
