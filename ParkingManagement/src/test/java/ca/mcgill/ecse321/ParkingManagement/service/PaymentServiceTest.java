7a9c4ce1f92034d3ed9b9938c2d8f12a54c3eec8
Ralph Choucha <112570592+RalphChoucha@users.noreply.github.com>
2023-03-28 23:08:15 -0400

    payment service test updates

--- ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/PaymentServiceTest.java
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/PaymentServiceTest.java
@@ -5,18 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 
 import ca.mcgill.ecse321.ParkingManagement.dao.*;
 import ca.mcgill.ecse321.ParkingManagement.model.*;
 
+@ExtendWith(MockitoExtension.class)
 public class PaymentServiceTest {
 
+    @Mock
+    private SystemInfoRepository systemInfoDao;
+
+    @InjectMocks
     private PaymentService service;
-    private SystemInfoRepository systemInfo;
-    private RegularTempSpot regSpot;
 
     private final String cardNumber = "1234567898765432";
     private final int hours = 1;
+    private final int id = 1;
+
 
     @Test
     public void testValidatePayment() {
@@ -32,8 +42,10 @@ public class PaymentServiceTest {
     @Test
     public void testGetCalculatedPriceForSpot() {
         int result = 0;
+
+        RegularTempSpot regSpot = new RegularTempSpot();
         result = service.getCalculatedPriceForSpot(regSpot, hours);
         assertNotEquals(0, result);
-        assertEquals(systemInfo.findSystemInfoById(1).getRegTempSpotPrice() * hours * 4, result);
+        assertEquals(systemInfoDao.findSystemInfoById(id).getRegTempSpotPrice() * hours * 4, result);
     }
 }
