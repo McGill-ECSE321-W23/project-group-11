215701fc1690fe60f4f780bb4f1fa78fe4f8e239
Brian Morava <zmorava515@hotmail.com>
2023-03-29 12:44:14 -0400

    login service test assertions

--- ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/LoginServiceTest.java
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/LoginServiceTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -25,19 +27,11 @@ public class LoginServiceTest {
 
     @InjectMocks
 	private LoginService service;
-
-
-    @BeforeEach
-	public void clearDatabase() {//not needed?
-		accountRepository.deleteAll();
-	}
-
-
-    //null test cases
+    
     //test cases of not existing in the database
 
     @Test
-    public void testValidLogin() {//account that hasn't already logged in should be able to login
+    public void testValidLogin() {//account that hasn't already logged in and exists in the database should be able to login
         
         AccountDto accountDto = new AccountDto("brianMorava123@gmail.com", "password", false);
         Account account = new Account();
@@ -45,50 +39,77 @@ public class LoginServiceTest {
         account.setPassword(accountDto.getPassword());
         when(accountRepository.existsByEmail(any(String.class))).thenReturn(true);
         when(accountRepository.findAccountByEmail(any(String.class))).thenReturn(account);
+        boolean output = false;
+
         try {
-            service.loginAccount(accountDto);
+            output = service.loginAccount(accountDto);
         } catch (Exception e) {
             fail("Should not throw any exception");
         }
+        assertTrue(output);
     }
 
 
     @Test
     public void testInvalidLogin() {//account that is already logged in shouldn't be able to login
-        assertThrows(Exception.class,
+    Exception e = assertThrows(Exception.class,
                 () -> {
                     AccountDto accountDto = new AccountDto("brianMorava123@gmail.com", "password", true);
                     service.loginAccount(accountDto);
                 }
         );
-
+        assertEquals(e.getMessage(),"Account is already logged in.");
     }
 
 
     @Test
-    public void testValidLogout() {//account that is already logged in should be able to logout
+    public void testInvalidLogin2() {//account that is null shouldn't be able to login
+    Exception e = assertThrows(Exception.class,
+                () -> {
+                    AccountDto accountDto = null;
+                    service.loginAccount(accountDto);
+                }
+        );
+        assertEquals(e.getMessage(),"Account cannot be null.");
+    }
+
+
+    @Test
+    public void testValidLogout() {//account that is logged in and exists in the database should be able to logout
        
         AccountDto accountDto = new AccountDto("brian@gmail.com", "password1", true);
         when(accountRepository.existsByEmail(any(String.class))).thenReturn(true);
-
+        boolean output = true;
         try {
-            service.logoutAccount(accountDto);
+            output = service.logoutAccount(accountDto);
         } catch (Exception e) {
             fail("Should not throw any exception");
         }
+        assertFalse(output);
     }
 
 
 
     @Test
     public void testInvalidLogout() {//account that is already logged out shouldn't be able to logout
-        assertThrows(Exception.class,
+    Exception e = assertThrows(Exception.class,
                 () -> {
                     AccountDto accountDto = new AccountDto("brianMorava123@gmail.com", "password", false);
                     service.logoutAccount(accountDto);
                 }
         );
+        assertEquals(e.getMessage(),"Account is already logged out.");
+    }
 
+    @Test
+    public void testInvalidLogout2() {//account that is null shouldn't be able to logout
+    Exception e = assertThrows(Exception.class,
+                () -> {
+                    AccountDto accountDto = null;
+                    service.logoutAccount(accountDto);
+                }
+        );
+        assertEquals(e.getMessage(),"Account cannot be null.");
     }
     
 }
