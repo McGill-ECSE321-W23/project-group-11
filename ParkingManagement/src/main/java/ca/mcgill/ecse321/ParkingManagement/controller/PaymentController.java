5957936a2a2637ec6b7b290dc58cffe38904fe07
Ralph Choucha <112570592+RalphChoucha@users.noreply.github.com>
2023-03-29 21:54:26 -0400

    PaymentService Fixes and Controller Implementation

---
+++ ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/controller/PaymentController.java
@@ -0,0 +1,51 @@
+package ca.mcgill.ecse321.ParkingManagement.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ca.mcgill.ecse321.ParkingManagement.service.*;
+import ca.mcgill.ecse321.ParkingManagement.dto.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class PaymentController {
+
+    @Autowired
+    PaymentService paymentService;
+    
+    
+    @GetMapping(value ={"/price/temp", "/price/temp/"})
+    public ResponseEntity<?> getTempSpotPrice(@RequestBody TempSpotDto spot){
+        return new ResponseEntity<>(paymentService.getCalculatedPriceForSpot(spot), HttpStatus.OK);
+    }
+
+    @GetMapping(value ={"/price/month", "/price/month/"})
+    public ResponseEntity<?> getMonthlySpotPrice(){
+            return new ResponseEntity<>(paymentService.getPriceForMonthlySpot(), HttpStatus.OK);
+    }
+
+    @GetMapping(value ={"/price/service/{name}", "/price/service/{name}/"})
+    public ResponseEntity<?> getServicePrice(@PathVariable String name) throws Exception{
+        try{
+            return new ResponseEntity<>(paymentService.getPriceForService(name), HttpStatus.OK);
+        } catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+        
+    }
+
+    @GetMapping(value ={"/payment/{cardNumber}", "/payment/{cardNumber}/"})
+    public ResponseEntity<?> getPaymentValidation(@PathVariable String cardNumber) throws Exception{
+        String result = "";
+        try{
+            paymentService.validatePayment(cardNumber);
+            result = "Payment Validated";
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
--- ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/service/PaymentService.java
+++ ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/service/PaymentService.java
@@ -1,22 +1,30 @@
 package ca.mcgill.ecse321.ParkingManagement.service;
 
 import ca.mcgill.ecse321.ParkingManagement.model.*;
+import ca.mcgill.ecse321.ParkingManagement.dao.ServiceTypeRepository;
+import ca.mcgill.ecse321.ParkingManagement.dao.SystemInfoRepository;
 import ca.mcgill.ecse321.ParkingManagement.dto.*;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class PaymentService {
+
+    @Autowired
+    SystemInfoRepository sysInfo;
+
+    @Autowired
+    ServiceTypeRepository serviceType;
 
     /**
      * Method to validate payment for a spot
      * 
      * @param cardNumber A String that represents the customer's card number
      * @return True if the payment is valid
-     * @throws Exception If any syntax error exists in the card number string 
+     * @throws Exception If any syntax error exists in the card number string
      */
     @Transactional
     public boolean validatePayment(String cardNumber) throws Exception {
@@ -30,12 +38,12 @@ public class PaymentService {
             Exception e = new Exception("Card number must be 16 digits long");
             throw e;
         }
-        
-        for(int i = 0; i<cardNumber.length(); i++) {
-			if(!Character.isDigit(cardNumber.charAt(i))) {
-				Exception e = new Exception("Card number must only contain numbers");
-				throw e;
-			}
+
+        for (int i = 0; i < cardNumber.length(); i++) {
+            if (!Character.isDigit(cardNumber.charAt(i))) {
+                Exception e = new Exception("Card number must only contain numbers");
+                throw e;
+            }
         }
         return true;
     }
@@ -43,48 +51,55 @@ public class PaymentService {
     /**
      * Method that calculates the price the customer needs to pay for the spot
      * 
-     * @param spot  The particular spot that the customer is taking
-     * @param hours The number of hours the customer is occupying the spot
-     * @param sysInfo The particular system info instance in order to get the pricing information
+     * @param spot The particular spot that the customer is taking
      * @return The integer that represents the total cost that the customer has to
      *         pay
      */
     @Transactional
-    public int getCalculatedPriceForSpot(TempSpotDto spot, int hours, SystemInfoDto sysInfo) {
+    public int getCalculatedPriceForSpot(TempSpotDto spot) {
         int price = 0;
         if (spot.getCarDto().getSize().equals(Size.Regular)) {
-                price += sysInfo.getRegTempSpotPrice() * (hours * 4);
-                return price;
+            price += sysInfo.findSystemInfoById(1).getRegTempSpotPrice() * (spot.getDuration());
+            return price;
 
         }
 
         if (spot.getCarDto().getSize().equals(Size.Large)) {
-                price += sysInfo.getLargeTempSpotPrice() * (hours * 4);
-                return price;
+            price += sysInfo.findSystemInfoById(1).getLargeTempSpotPrice() * (spot.getDuration());
+            return price;
         }
         return price;
     }
 
     /**
-     * Method to get the price for a monthly reserved spot 
-     * @param sysInfo The particular system info instance in order to get the pricing information
+     * Method to get the price for a monthly reserved spot
+     * 
      * @return The price associated with the monthly spot
      */
     @Transactional
-    public int getPriceForMonthlySpot(SystemInfoDto sysInfo){
-        int price = sysInfo.getReservedSpotPrice();
+    public int getPriceForMonthlySpot() {
+        int price = sysInfo.findSystemInfoById(1).getReservedSpotPrice();
         return price;
     }
 
     /**
      * Method to get The price for a given service type
-     * @param serviceType The particular service type whose pricing information is needed 
+     * 
+     * @param name The particular service type's name whose pricing information is
+     *             needed
      * @return The price associated with the service
+     * @throws Exception if the service type is null
      */
     @Transactional
-    public int getPriceForService(ServiceTypeDto serviceType){
-        int price = serviceType.getCost();
-        return price;
+    public int getPriceForService(String name) throws Exception {
+        if (serviceType.findServiceTypeByName(name) != null) {
+            int price = serviceType.findServiceTypeByName(name).getCost();
+            return price;
+        } else {
+            Exception e = new Exception("Service Type is null");
+            throw e;
+        }
+
     }
 
 }
