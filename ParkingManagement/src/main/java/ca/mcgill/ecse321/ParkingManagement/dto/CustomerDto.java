5bcf6b1d50c133094d71b06476e438fa0f096e3b
Ralph Choucha <112570592+RalphChoucha@users.noreply.github.com>
2023-04-07 14:19:29 -0400

    Customer and Employee Dtos Created

---
+++ ParkingManagement/src/main/java/ca/mcgill/ecse321/ParkingManagement/dto/CustomerDto.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.ParkingManagement.dto;
+
+public class CustomerDto {
+    
+    public int id;
+    public String account;
+
+    /**
+     * Dto constructor
+     * @param id customer id
+     * @param account email identifier for account
+     * 
+     */
+    
+    public CustomerDto(int id, String account) {
+        this.id = id;
+        this.account = account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return this.account;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
