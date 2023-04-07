4eb101e400a47a041f1d650dd3b37cd60a74f1b3
Jack Zaarour <jwzaarour@gmail.com>
2023-04-06 21:28:28 -0400

    Style edits

--- ParkingManagement-frontend/src/components/BookCarService.vue
+++ ParkingManagement-frontend/src/components/BookCarService.vue
@@ -64,9 +64,99 @@
 <script>
 </script>
 <style>
-    #homePage{
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        color: #2c3e50;
-        background: #f2ece8;
+  html, body {
+    margin: 0;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  #bookCarService {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    color: #333;
+    background: #f9f9f9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 40px;
+    box-sizing: border-box;
+  }
+
+  h2 {
+    margin-bottom: 30px;
+    font-weight: 500;
+  }
+
+  .reservation-form {
+    display: grid;
+    grid-template-columns: repeat(2, auto);
+    grid-gap: 20px;
+    align-items: center;
+  }
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 5px;
+    font-weight: 500;
+  }
+
+  input,
+  select {
+    padding: 12px;
+    font-size: 16px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    background-color: #fff;
+    color: #333;
+    outline: none;
+    -webkit-appearance: none;
+  }
+
+  input:focus,
+  select:focus {
+    box-shadow: 0 0 0 2px #007aff;
+  }
+
+  .confirm-btn {
+    grid-column: 1 / -1;
+    background-color: #007aff;
+    color: white;
+    padding: 12px 20px;
+    font-size: 16px;
+    font-weight: 500;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .confirm-btn:hover {
+    background-color: #0058d3;
+  }
+
+  .error-message {
+    color: #ff3b30;
+    margin-top: 20px;
+    font-weight: 500;
+  }
+
+  button {
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        background-color: #007bff;
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+        margin-right: 10px;
+    }
+
+    button:hover {
+    background-color: #0069d9;
     }
 </style>
