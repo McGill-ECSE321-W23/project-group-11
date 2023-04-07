2399bf622569100bc6573c05bcd37d3cdfefeae5
Jack Zaarour <jwzaarour@gmail.com>
2023-04-06 20:03:14 -0400

    Added the MonthlyParking page

--- ParkingManagement-frontend/src/router/index.js
+++ ParkingManagement-frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import MonthlyParking from '@/components/MonthlyParking'
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },    
+    {
+      path: '/MonthlyPark',
+      name: 'MonthlyParking',
+      component: MonthlyParking
     }
   ]
 })
