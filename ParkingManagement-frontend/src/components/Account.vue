dc22a60a4bd0210f00e16e67ad81bb98174ee594
Ralph Choucha <112570592+RalphChoucha@users.noreply.github.com>
2023-04-07 12:31:16 -0400

    Create account page updates

--- ParkingManagement-frontend/src/components/Account.vue
+++ ParkingManagement-frontend/src/components/Account.vue
@@ -19,7 +19,8 @@
 <template>
     <div>
     <center>
-        <h1>Create Account</h1>
+        <h1>ParkingPlus&nbsp<img src="https://static.wixstatic.com/media/cef2a4_5488e99ac9bd419e9f05722ba154e95a~mv2.png/v1/fill/w_560,h_554,al_c,lg_1,q_85,enc_auto/ParkPlus%20Icon%20CMYK%20highres.png" alt="ParkingPlus Logo" style="width: 80px; height: 80px; margin-right: 20px;"><br></h1>
+        <h2>Create Account</h2>
         <h4>Welcome to the account creation page</h4><br><br>
 
         <label for="email">Enter your email:</label>
@@ -27,6 +28,9 @@
 
         <label for="password">Enter your password:</label>
         <input type="password" id="password" name="password"><br><br>
+
+        <label for="reenter-password">Re-enter your password:</label>
+        <input type="password" id="reenter-password" name="reenter-password"><br><br>
 
         <label for="logintype">Registering as a:</label>
         <select name="Type" id="logintype">
@@ -37,8 +41,8 @@
 
         <button type="submit">Create Account</button><br><br>
 
-        <Label for="backtologin">Already have an account?</label>
-        <button type="submit">Login Instead</button>
+        <p>Already have an account?  <a href="#">Login Instead</a></p>
+        
     </center>
     </div>
 </template>
