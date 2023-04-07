9a0952cd843011d29734fd4d77da460428cc4245
LukeBebee <ljbebee@gmail.com>
2023-04-06 22:43:45 -0400

    car controller and service done

---
+++ ParkingManagement/src/test/java/ca/mcgill/ecse321/ParkingManagement/service/CarServiceTest.java
@@ -0,0 +1,126 @@
+package ca.mcgill.ecse321.ParkingManagement.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyInt;import static org.mockito.Mockito.lenient;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+// import java.time.format.DateTimeFormatter;
+// import java.util.Calendar;
+// import java.util.ArrayList;
+// import java.util.List;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+// import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+import ca.mcgill.ecse321.ParkingManagement.dao.CarRepository;
+import ca.mcgill.ecse321.ParkingManagement.dto.CarDto;
+import ca.mcgill.ecse321.ParkingManagement.model.Car;
+import ca.mcgill.ecse321.ParkingManagement.model.Size;
+import ca.mcgill.ecse321.ParkingManagement.utility.DtoConverters;
+
+@ExtendWith(MockitoExtension.class)
+public class CarServiceTest {
+
+    @Mock
+    private CarRepository carRepository;
+
+    @InjectMocks
+    private CarService service;
+
+    private final String CAR_KEY = "plate_number";
+
+    @BeforeEach
+    public void setMockOutput() {
+        lenient().when(carRepository.existsBylicensePlate(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+            if(invocation.getArgument(0).equals(CAR_KEY)) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+        lenient().when(carRepository.findCarBylicensePlate(anyString())).thenAnswer( (InvocationOnMock invocation) -> {
+            if(invocation.getArgument(0).equals(CAR_KEY)) {
+                Car car = new Car();
+                car.setSize(Size.Regular);
+                car.setLicensePlate(CAR_KEY);
+                return car;
+            } else {
+                return null;
+            }
+        });
+    
+        // Whenever anything is saved, just return the parameter object
+        Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+            return invocation.getArgument(0);
+        };
+        lenient().when(carRepository.save(any(Car.class))).thenAnswer(returnParameterAsAnswer);
+    }
+
+    @Test
+    public void testCreateCar() {
+        String error = "";
+        CarDto carDto = new CarDto(CAR_KEY, Size.Regular);
+
+        try {
+            service.createCar(carDto);
+        } catch (Exception e) {
+            error += e.getMessage();
+        }
+        assertEquals("", error);
+    }
+
+    @Test
+    public void testCreateCarInvalidInputs() {
+        String error = "";
+        CarDto carDto = new CarDto("", Size.Regular);
+        try {
+            service.createCar(carDto);
+        } catch (Exception e) {
+            error += e.getMessage();
+        }
+        assertEquals("Invalid car inputs", error);
+    }
+
+    @Test
+    public void testDeleteCar() {
+        String error = "";
+        CarDto carDto = new CarDto(CAR_KEY, Size.Regular);
+        try {
+            service.deleteCar(carDto);
+        } catch (Exception e) {
+            error += e.getMessage();
+        }
+        assertEquals("", error);
+    }
+
+    @Test
+    public void testDeleteNonexistentCar() {
+        String error = "";
+        CarDto carDto = new CarDto("nonexistent plate", Size.Regular);
+        try {
+            service.deleteCar(carDto);
+        } catch (Exception e) {
+            error += e.getMessage();
+        }
+        assertEquals("Car with this plate number does not exist", error);
+    }
+}
