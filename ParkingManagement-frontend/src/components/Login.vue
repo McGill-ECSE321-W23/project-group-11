797b7da2495aef2d1bf5e4ae9aa652c749b7aa3a
Ralph Choucha <112570592+RalphChoucha@users.noreply.github.com>
2023-04-09 16:44:33 -0400

    login.vue updated create account redirect

--- ParkingManagement-frontend/src/components/Login.vue
+++ ParkingManagement-frontend/src/components/Login.vue
@@ -1,29 +1,27 @@
 <template>
-  <div>
-    <center>
-      <h1>ParkSimple&nbsp<img src="https://static.wixstatic.com/media/cef2a4_5488e99ac9bd419e9f05722ba154e95a~mv2.png/v1/fill/w_560,h_554,al_c,lg_1,q_85,enc_auto/ParkPlus%20Icon%20CMYK%20highres.png" alt="ParkSimple Logo" style="width: 80px; height: 80px; margin-right: 20px;"><br><br></h1>
-      
-      <label>Enter your email:</label>
-      <input type="text" v-model="accountEmail" placeholder="Email"><br><br>
-  
-      <label>Enter your password:</label>
-      <input type="text" v-model="accountPassword" placeholder="Password"><br><br>
-      
-      <button @click="login(accountEmail, accountPassword)">Login</button>
-  
-      <p style="color:red"><br><br>{{ errorMessage }}</p>
-  
-      <label><br><br>Don't have an account yet?</label>
-      <a href="/#/CreateAccount"><button>Create Account</button></a>
-  
-      <div class="manager-signin-container">
-        <router-link to="/ManagerLogin">
-          <button class="manager-signin-btn">Manager Sign In</button>
-        </router-link>
-      </div>
-  
-    </center>
-  </div>
+<div>
+  <center>
+    <h1>ParkingPlus&nbsp<img src="https://static.wixstatic.com/media/cef2a4_5488e99ac9bd419e9f05722ba154e95a~mv2.png/v1/fill/w_560,h_554,al_c,lg_1,q_85,enc_auto/ParkPlus%20Icon%20CMYK%20highres.png" alt="ParkingPlus Logo" style="width: 80px; height: 80px; margin-right: 20px;"><br><br></h1>
+    
+    <label for="email">Enter your email:</label>
+    <input type="email" id="email" name="email"><br><br>
+
+    <label for="password">Enter your password:</label>
+    <input type="password" id="password" name="password"><br><br>
+
+    <button type="submit">Login</button>
+
+    <label><br><br>Don't have an account yet?</label>
+    <a href="/#/Account"><button>Create Account</button></a>
+
+    <div class="manager-signin-container">
+      <a href="/#/ManagerLogin">
+        <button class="manager-signin-btn">Manager Sign In</button>
+      </a>
+    </div>
+
+  </center>
+</div>
 </template>
 
 <script>
